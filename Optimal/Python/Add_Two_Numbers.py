0ae917fb7b7255330e3fee8289c652c37e02c104
AcrobaticRaccoon47 <145118070+cc2803@users.noreply.github.com>
2024-11-04 18:53:58 +0530

    Create Add_Two_Numbers.py (Optimal)

---
+++ Optimal/Python/Add_Two_Numbers.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        start_node = ListNode()
+        tail = start_node
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sum  = val1 + val2 + carry
+            
+            digit = sum  % 10
+            carry = sum // 10
+            
+            tail.next = ListNode(digit)
+            tail = tail.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        result = start_node.next if start_node.next else start_node
+        return result
