ffa417b4aa5d27f1871322eee014a22c76e78030
cc2803 <145118070+cc2803@users.noreply.github.com>
2024-11-18 15:26:29 +0530

    update pointer_examples.cpp

--- Pre_requisites/pointer_examples.cpp
+++ Pre_requisites/pointer_examples.cpp
@@ -6,7 +6,9 @@ int main() {
     int a=30;
     int *aptr = &a;
     int **dptr = &aptr;
-    
+
+    int array[5]={10,200,38,40,450};
+
     cout<<a<<endl; //prints direct value of "a", i.e 30
     cout<<aptr<<endl; //prints address of "a", "aptr" variable stores address of variable "a"
     cout<<dptr<<endl; //prints address of pointer "aptr", i.e, whatever value is stored in just "aptr", (NOT *aptr)
@@ -14,5 +16,7 @@ int main() {
     cout<<*dptr<<endl;//DE-REFERENCING of dptr, value pointed by dptr is printed = aptr = address of a
     cout<<**dptr<<endl;//Double DE-REFERENCING of dptr, first dptr points at aptr, next aptr points at a, hence output is value of a = 30
     
+    cout<<*(array)<<endl; // accessing array elements via pointers, here, it points to index 0,i.e 1st element (10)
+    cout<<*(array+3)<<endl; // 0+3=3rd index, or 4th element accessed, i.e 40
     return 0;
 }
