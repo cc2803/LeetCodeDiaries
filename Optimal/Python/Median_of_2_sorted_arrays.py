79bf9b711a7481136963317c05e0a7064ee132e7
AcrobaticRaccoon47 <145118070+cc2803@users.noreply.github.com>
2024-11-23 21:21:21 +0530

    Create Median_of_2_sorted_arrays.py

---
+++ Optimal/Python/Median_of_2_sorted_arrays.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        mega = nums1+nums2
+        mega.sort()
+        mlen=len(mega)
+
+        if len(mega)%2!=0:
+            median=mega[mlen//2]
+        else:
+            median=(mega[(mlen//2)]+mega[mlen//2 -1])/2.0
+    
+        return median
